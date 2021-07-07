8122f1aed3d96424cb2b3354356c1e23eaa6ab77
leenawon <nawon5154@gmail.com>
2021-07-07 19:48:34 +0900

    입력받은 사용자 이름 이용 인사말 표시

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,10 +1,19 @@
 const loginForm = document.querySelector('.loginForm');
+
 const loginInput = document.querySelector('.loginForm input');
 
+const welcomeMessage = document.querySelector('.welcome');
+
+const HIDDEN_CLASS = 'hidden';
+
 function handleLoginFormSubmit(event) {
-  const userNameValue = loginInput.value;
   event.preventDefault();
-  console.log(userNameValue);
+
+  loginForm.classList.add(HIDDEN_CLASS);
+  const userNameValue = loginInput.value;
+  
+  welcomeMessage.classList.remove(HIDDEN_CLASS);
+  welcomeMessage.innerText = `안녕하세요! ${userNameValue}님`;
 }
 
 loginForm.addEventListener('submit', handleLoginFormSubmit);
