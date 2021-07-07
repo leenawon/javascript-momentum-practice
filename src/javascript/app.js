5a2217251d4c617cbdf7d023b16b4a6c5bd7ecec
leenawon <nawon5154@gmail.com>
2021-07-08 02:04:12 +0900

    localStorage에 사용자 이름 유무 확인 추가

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -6,15 +6,26 @@ const welcomeMessage = document.querySelector('.welcome');
 
 const HIDDEN_CLASS = 'hidden';
 
-function handleLoginFormSubmit(event) {
-  event.preventDefault();
+const USER_NAME = 'userNameValue';
 
+function handleLoginSubmit(event) {
+  event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASS);
   const userNameValue = loginInput.value;
+  localStorage.setItem(USER_NAME, userNameValue);
+  showWelcomeMessage(userNameValue);
+}
 
-  localStorage.setItem("userNameValue",userNameValue);
-  welcomeMessage.innerText = `안녕하세요! ${userNameValue}님`;
+function showWelcomeMessage(userName) {
+  welcomeMessage.innerText = `안녕하세요! ${userName}님`;
   welcomeMessage.classList.remove(HIDDEN_CLASS);
 }
 
-loginForm.addEventListener('submit', handleLoginFormSubmit);
+const getUserName = localStorage.getItem(USER_NAME);
+
+if(getUserName === null) {
+  loginForm.classList.remove(HIDDEN_CLASS);
+  loginForm.addEventListener('submit', handleLoginSubmit);
+} else {
+  showWelcomeMessage(getUserName);
+}
