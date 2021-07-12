e00e08fe1c3d6c0a4a405fe989ae2f04e8317911
leenawon <nawon5154@gmail.com>
2021-07-12 15:32:03 +0900

    삭제 버튼 추가하고 리스트 삭제 기능 추가 (todos.js)

--- src/javascript/todos.js
+++ src/javascript/todos.js
@@ -2,19 +2,29 @@ const todoForm = document.querySelector('.todoForm');
 const todoInput = todoForm.querySelector('input');
 const todoList = document.querySelector('ul.todoList');
 
-function handleTodoFormSubmit(event) {
-  event.preventDefault();
-  const todoValue = todoInput.value;
-  todoInput.value = "";
-  showTodo(todoValue);
+function handleDeleteButton(event) {
+  const deleteListItem = event.target.parentElement;
+  deleteListItem.remove();
 }
 
 function showTodo(todo) {
   const listItem = document.createElement('li');
   const span = document.createElement('span');
-  listItem.appendChild(span);
   span.innerText = todo;
+  const deleteButton = document.createElement('button');
+  deleteButton.innerText = '❌';
+
+  deleteButton.addEventListener('click', handleDeleteButton);
+  listItem.appendChild(span);
+  listItem.appendChild(deleteButton);
   todoList.appendChild(listItem);
+}
+
+function handleTodoFormSubmit(event) {
+  event.preventDefault();
+  const todoValue = todoInput.value;
+  todoInput.value = "";
+  showTodo(todoValue);
 }
 
 todoForm.addEventListener('submit', handleTodoFormSubmit);
