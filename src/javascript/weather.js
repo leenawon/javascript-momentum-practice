b5745caac62df36516636117ce7448bf612a840a
leenawon <nawon5154@gmail.com>
2021-07-15 02:16:04 +0900

    openweathermap 이용 날씨와 장소 보여주게 추가 (weather.js)

--- src/javascript/weather.js
+++ src/javascript/weather.js
@@ -1,7 +1,19 @@
+const API_KEY = "aa9e0fa890eadb16c5a83e85ecfbbf26";
+
 function successGeolocation(position) {
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
-  console.log(latitude, longitude);
+  const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
+  fetch(weatherURL).then((response) => response.json()).then((data) => {
+    const locationSpan = document.querySelector('div.weather span:first-child');
+    const weatherSpan = document.querySelector('div.weather span:last-child');
+    const location = data.name;
+    const weather = data.weather[0].main;
+    const temp = data.main.temp;
+
+    locationSpan.innerText = location;
+    weatherSpan.innerText = `${weather} / 기온 : ${temp}`;
+  });
 }
 
 function errorGeolocation() {
